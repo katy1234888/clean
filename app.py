ad4010783d4481b7967825ce24e54078f8e49cad
katy1234888 <katyayani.kul@gmail.com>
2026-05-10 00:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ try:
 
 except Exception as e:
     st.warning(f"Storyboard Error: {e}")
-if st.session_state.page == 2:
+if st.session_state.page == 4:
 
     # =========================================================
     # 📘 DETAILED ANSWERS (WORD-BY-WORD DISPLAY)
